a0d86aa4b7540a23c2af8b1fd71a4c1f2915821d
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2021-04-01 20:18:52 -0300

    Configuração do banco de dados e typeorm

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
 const port = 3333;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   return res.json({ message: "Servidor rodando!" });
