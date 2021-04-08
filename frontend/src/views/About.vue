537554d02ed1558a94e186a1c78f91b55883e5af
NardoHZ <48166282+NardoHZ@users.noreply.github.com>
2021-04-07 22:32:15 -0300

    TesteCSS

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="about">
       <h1>Sobre</h1>
+        <div class="cabeçalho">
+          <h1>Sublim</h1>
+        </div>
+         <div class="rodape">
+          <h1>Desenvolvido pela Sublime</h1>
+        </div>
+
     </div>
 </template>
 
@@ -8,6 +15,32 @@
 .about {
   width: 100%;
   height: 100vh;
-  background-color: #000;
+  background-color: #470EAE;
 }
+h1{
+  color: rgb(255, 255, 255);
+ 
+
+}
+.cabeçalho{
+  position: absolute;
+width: 100%;
+height: 124px;
+left: 0px;
+top: 0px;
+
+
+background: #5F07FF;
+}
+.rodape{
+  position: absolute;
+width: 100%;
+height: 124px;
+left: 0px;
+top: 900px;
+
+
+background: #5F07FF;
+}
+
 </style>
