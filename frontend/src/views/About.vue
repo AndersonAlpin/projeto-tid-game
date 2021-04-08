d9a8534032842931583e2c09ec5b29a2c8a0355a
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2021-04-08 09:49:46 -0300

    Criação da pasta assets
    Local onde ficará o css e as imagens

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,46 +1,40 @@
 <template>
-    <div class="about">
-      <h1>Sobre</h1>
-        <div class="cabeçalho">
-          <h1>Sublim</h1>
-        </div>
-         <div class="rodape">
-          <h1>Desenvolvido pela Sublime</h1>
-        </div>
-
+  <div class="about">
+    <h1>Sobre</h1>
+    <div class="cabecalho">
+      <h1>Sublime</h1>
     </div>
+    <div class="rodape">
+      <h1>Desenvolvido pela Sublime</h1>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .about {
   width: 100%;
   height: 100vh;
-  background-color: #470EAE;
+  background-color: #470eae;
 }
-h1{
+h1 {
   color: rgb(255, 255, 255);
- 
-
 }
-.cabeçalho{
+.cabecalho {
   position: absolute;
-width: 100%;
-height: 124px;
-left: 0px;
-top: 0px;
+  width: 100%;
+  height: 124px;
+  left: 0px;
+  top: 0px;
 
-
-background: #5F07FF;
+  background: #5f07ff;
 }
-.rodape{
+.rodape {
   position: absolute;
-width: 100%;
-height: 124px;
-left: 0px;
-top: 900px;
+  width: 100%;
+  height: 124px;
+  left: 0px;
+  top: 900px;
 
-
-background: #5F07FF;
+  background: #5f07ff;
 }
-
 </style>
