12177eda00a705415ff41b7ecbcdbb2e35886af5
TheAldo <47190980+TheAldo@users.noreply.github.com>
2021-04-11 14:41:11 -0300

    tela menu
    tela menu

--- frontend/src/views/Menu.vue
+++ frontend/src/views/Menu.vue
@@ -1,5 +1,178 @@
 <template>
-  <div class="Menu">
-    <h1>Menu</h1>
+  <div class="Menu" id="menu">
+       <div id="barra">
+       </div>
+       <div id="barrameio">
+          <h1 id="bemvindo">Bem vindo!</h1>
+          <h2 id="opc">Qual tema deseja jogar?</h2>
+          <button id="butão1">tecnologia</button>
+          <button id="butão2">Area medica</button>
+          <button id="butão3">Area historiaca</button>
+          <img src="../img/logo.jpg" id="logo1">
+          
+       </div>
+       <div id="barrabaixo"></div>
+
+       
+  
   </div>
+
+  
 </template>
+
+
+<script>
+
+  
+</script>
+
+
+<style>
+
+#menu {
+
+
+  position: absolute;
+width: 1500px;
+height: 819px;
+left: 0px;
+top: 60px;
+
+background: #470EAE;
+}
+#barra{
+position: absolute;
+width: 1500px;
+height: 124px;
+left: 0px;
+top: -100px;
+
+background: #5F07FF;
+
+
+}
+#barrameio{
+  position: absolute;
+width: 450px;
+height: 510px;
+left: 489px;
+top: 70px;
+
+background: #5F07FF;
+border-radius: 41px;
+} 
+#bemvindo{
+  position: absolute;
+width: 190px;
+height: 41px;
+left: 120px;
+top: 20px;
+
+font-family: Roboto;
+font-style: normal;
+font-weight: bold;
+font-size: 30px;
+line-height: 41px;
+/* identical to box height */
+
+text-transform: uppercase;
+
+color: #FFFFFF;
+
+
+}
+#opc{
+
+  position: absolute;
+width: 300px;
+height: 25px;
+left: 70px;
+top: 100px;
+
+font-family: Roboto;
+font-style: normal;
+font-weight: bold;
+font-size: 20px;
+line-height: 29px;
+text-transform: uppercase;
+
+color: #FFFFFF;
+}
+#barrabaixo{
+
+  position: absolute;
+width: 1500px;
+height: 107px;
+left: 0px;
+top: 810px;
+
+background: #5F07FF;
+}
+#butão1{
+position: absolute;
+width: 334px;
+height: 67px;
+left: 70px;
+top: 150px;
+color: #FFFFFF;
+font-family: Roboto;
+font-style: normal;
+font-weight: bold;
+font-size: 25px;
+line-height: 29px;
+text-transform: uppercase;
+
+
+background: #470EAE;
+border-radius: 14px;
+
+}
+#butão2{
+position: absolute;
+width: 334px;
+height: 67px;
+left: 70px;
+top: 250px;
+color: #FFFFFF;
+font-family: Roboto;
+font-style: normal;
+font-weight: bold;
+font-size: 25px;
+line-height: 29px;
+text-transform: uppercase;
+
+background: #470EAE;
+border-radius: 14px;
+
+}
+
+#butão3{
+position: absolute;
+width: 334px;
+height: 67px;
+left: 70px;
+top: 350px;
+color: #FFFFFF;
+font-family: Roboto;
+font-style: normal;
+font-weight: bold;
+font-size: 25px;
+line-height: 29px;
+text-transform: uppercase;
+
+
+background: #470EAE;
+border-radius: 14px;
+
+}
+#logo1{
+
+  position: absolute;
+left: -120.14%;
+right: 27.4%;
+top: 2.26%;
+bottom: 24.39%;
+}
+
+
+</style>
