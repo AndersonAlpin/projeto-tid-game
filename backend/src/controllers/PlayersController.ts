dc38aa6a5fa5aaa57d6d697de743f156aaa7615c
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2021-04-14 12:26:43 -0300

    Update PlayersController.ts

--- backend/src/controllers/PlayersController.ts
+++ backend/src/controllers/PlayersController.ts
@@ -3,6 +3,7 @@ import { getRepository } from "typeorm";
 import Player from "../models/Player";
 
 export default {
+  // APENAS PARA TESTAR A FUNCIONALIDADE
   async index(request: Request, response: Response) {
     return response.json({ message: "Servidor rodando!" });
   }
