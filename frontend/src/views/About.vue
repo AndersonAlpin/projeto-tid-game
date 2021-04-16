b77697c4183d83a1e1a46d7ed1f7bcdef5441c7b
NardoHZ <48166282+NardoHZ@users.noreply.github.com>
2021-04-16 10:54:28 -0300

    TelaSobre

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,28 +1,24 @@
 <template>
   <div class="container" id="sobre">
     <div class="row">
-      <div id="titulo" class="col-12 col-sm-12"><h2>Sobre</h2></div>
-      <div class="col-12 col-sm-12">
-        <div id="texto">
-          Faz parte do Paleolítico, período compreendido entre de 4,4 milhões de
-          anos até 8000 a.C., o domínio do fogo pelo homem. A partir do momento
-          que o homem conseguiu utilizar a descoberta a seu favor, o fogo
-          tornou-se um alicerce para as civilizações. Vale lembrar que antes
-          disso, o homem já tinha observado o fogo em decorrência de fenômenos
-          naturais, como raios, que provocam incêndios, e erupções de vulcões. O
-          homo erectus foi o primeiro ancestral do ser humano a dominar o fogo,
-          utilizando pedras e madeira. Através do atrito entre duas pedras, a
-          faísca liberada servia como fonte de ignição para iniciar a chama. Com
-          o surgimento do fogo, os povos aprenderam a utilizá-lo para se
-          aquecer, cozinhar alimentos, espantar animais ferozes e iluminar
-          ambientes durante a noite. Como consequência também, a fabricação de
-          ferramentas de ferro foi possível com o desenvolvimento da metalurgia,
-          técnica descoberta com a moldagem do metal no fogo. Para os gregos,
-          existia o mito de que o fogo foi roubado dos deuses por Prometeu e
-          entregue aos homens. O filósofo grego Empédocles utilizou o fogo para
-          tentar explicar a composição da matéria, juntamente com o ar, a água e
-          a terra. Para Aristóteles, o fogo poderia ser distinguido dos demais
-          elementos pelas suas propriedades, quente e seco.
+      
+        <div id="titulo" class="col-12 col-sm-12"><h2>Sobre</h2></div>
+          <div class="col-12 col-sm-12">
+          <div id="texto">
+         O fulcro do jogo é desafiar o usuário a testar seus conhecimentos através de telas estáticas que estão concomitantes ao tema escolhido.
+         O usuário escolherá uma área de conhecimentos e a partir de cliques responderão questões clicando em imagens perdidas no cenário
+         Exemplo:
+         Em um cenário onde o tema escolhido foi tecnologia. O desafio seria o usuário encontrar a peça responsável por armazenar o sistema e arquivos.
+         Desafio:
+         A cada vitoria o jogo vai aumentar a dificuldade de imagens perdidas no cenario, fazendo que o jogador tenha mais raciocínio lógico e provocando tambem ele aprender mais sobre o tema.
+         Recompesa :
+         A cada vitoria o jogo vai da pontos e vai subir de nivel, jogador tambem poderar ganhar dicas.
+         Regras:
+         o jogador so vai poder subir de nivel depois que atigir determinados pontos.
+         Narrativa:
+         O jogo vai narrar assuntos como tecnologia, saude etc..
+         <div id="button1">
+          <a href="/menu.vue" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">jogar</a></div>
         </div>
       </div>
     </div>
@@ -51,5 +47,11 @@
   position: absolute;
   left: 100vh;
   top: 100px;
+}
+#button1{
+  
+   
+
+
 }
 </style>
