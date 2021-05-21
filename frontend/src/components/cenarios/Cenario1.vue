c519d540460375aa83fa0045027f93b7cb75b411
TheAldo <47190980+TheAldo@users.noreply.github.com>
2021-05-21 12:07:48 -0300

    Update Cenario1.vue

--- frontend/src/components/cenarios/Cenario1.vue
+++ frontend/src/components/cenarios/Cenario1.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>
-    
+ 
+ <div class="thumbnail-wrapper">
+  
  <img src="@/assets/images/cenarioum.jpg" id="fundo" />
+  
+  <button id="close" class="closing" onClick="javascript:close_clip()"><img src="@/assets/images/controle.png" /></button>
+ 
+ 
+ </div>
 
 
-  </div>
 </template>
 
 <script>
@@ -24,6 +29,24 @@ top: 72px;
 
 
 
+}
+
+.thumbnail-wrapper {
+    display:inline-block;
+    position:relative;
+}
+.thumbnail-wrapper button {
+    position:absolute;
+    z-index:1;
+    top:50%;
+    left:50%;
+    width:60px;               /* largura para o botão */
+    height:30px;              /* altura para o botão */
+    padding:0;
+    margin:5px 0 0 60px;   /* margem à esquerda e ao topo metade da medida do botão */
+    border:0 none;
+ 
+    
 }
 
 </style>
