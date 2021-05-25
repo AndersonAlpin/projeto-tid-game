a5d5f025dafc1b422632cc1b13e3c7e4444cd0d0
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2021-05-25 18:32:16 -0300

    Atualização da tela sobre

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,23 +1,40 @@
 <template>
   <div class="container" id="sobre">
     <div class="row">
-      
-        <div id="titulo" class="col-12 col-sm-12"><h2>Sobre</h2></div>
-          <div class="col-12 col-sm-12">
-          <div id="texto">
-         O fulcro do jogo é desafiar o usuário a testar seus conhecimentos através de telas estáticas que estão concomitantes ao tema escolhido.
-         O usuário escolherá uma área de conhecimentos e a partir de cliques responderão questões clicando em imagens perdidas no cenário
-         Exemplo:
-         Em um cenário onde o tema escolhido foi tecnologia. O desafio seria o usuário encontrar a peça responsável por armazenar o sistema e arquivos.
-         Desafio:
-         A cada vitoria o jogo vai aumentar a dificuldade de imagens perdidas no cenario, fazendo que o jogador tenha mais raciocínio lógico e provocando tambem ele aprender mais sobre o tema.
-         Recompesa :
-         A cada vitoria o jogo vai da pontos e vai subir de nivel, jogador tambem poderar ganhar dicas.
-         Regras:
-         o jogador so vai poder subir de nivel depois que atigir determinados pontos.
-         Narrativa:
-         O jogo vai narrar assuntos como tecnologia, saude etc..
-        
+      <div id="titulo" class="col-12 col-sm-12"><h2>Sobre</h2></div>
+      <div class="col-12 col-sm-12">
+        <div id="texto">
+          <p>
+            O fulcro do jogo é desafiar o usuário a testar seus conhecimentos
+            através de telas estáticas que estão concomitantes ao tema
+            escolhido.
+            O usuário escolherá uma área de conhecimentos e a partir de cliques
+            responderão questões clicando em imagens perdidas no cenário.
+          </p>
+          <p>Exemplo:</p>
+          <p>
+            Em um cenário onde o tema escolhido foi tecnologia. O desafio seria
+            o usuário encontrar a peça responsável por armazenar o sistema e
+            arquivos.
+          </p>
+          <p>Desafio:</p>
+          <p>
+            A cada vitoria o jogo vai aumentar a dificuldade de imagens perdidas
+            no cenario, fazendo que o jogador tenha mais raciocínio lógico e
+            provocando tambem ele aprender mais sobre o tema.
+          </p>
+          <p>Recompesa :</p>
+          <p>
+            A cada vitoria o jogo vai da pontos e vai subir de nivel, jogador
+            tambem poderar ganhar dicas.
+          </p>
+          <p>Regras:</p>
+          <p>
+            o jogador so vai poder subir de nivel depois que atigir determinados
+            pontos.
+          </p>
+          <p>Narrativa:</p>
+          <p>O jogo vai narrar assuntos como tecnologia, saude etc..</p>
         </div>
       </div>
     </div>
@@ -27,25 +44,17 @@
 <style scoped>
 #titulo {
   color: rgb(255, 255, 255);
+  text-align: center;
+  font-size: 40px;
+  text-transform: uppercase;
 }
+
 #texto {
   width: 100%;
-
   margin-top: 20px;
-
-  font-family: "Avenir", "Roboto", Arial;
+  font-size: 18px;
+  line-height: 22px;
   text-align: justify;
   color: rgb(255, 255, 255);
 }
-#image {
-  position: absolute;
-  left: -50px;
-  top: 120px;
-}
-#image2 {
-  position: absolute;
-  left: 100vh;
-  top: 100px;
-}
-
 </style>
