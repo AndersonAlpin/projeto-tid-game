6f78b44762561c6593fb322241c5704d9108eb30
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2021-05-25 13:05:19 -0300

    Tela de Login concluída

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,71 +1,98 @@
 <template>
-  <div class="container">
-    <div class="row">
+  <div id="register">
+    <div class="box">
+      <div class="logo">
+        <img src="@/assets/images/logo-2.png" alt="SUBLIME GAMING" />
+      </div>
+
       <form class="form">
-        <div class="emai">
-        <input id="email" type="email" placeholder="EMAIL:">
-          </div>
-          
-            <div class="senh">
-              <input id="senha" type="Password" placeholder="SENHA:">
-            </div>
-      </form> 
-       </div>
+        <input type="email" placeholder="E-MAIL" />
+        <input type="password" placeholder="SENHA" />
+
+        <div class="button-group">
+          <button class="btn button-entrar">ENTRAR</button>
+          <button class="btn button-cadastrar">CRIAR CADASTRO</button>
+        </div>
+      </form>
+    </div>
   </div>
-  
 </template>
 
+<script>
+export default {};
+</script>
+
 <style scoped>
-.container{
-   background: #5f07ff;
-  width: 300px;
-  height: 400px;
+#register {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.box {
+  position: relative;
+  width: 350px;
+  height: 450px;
   border-radius: 41px;
-  margin-top: 30px;
+  background-color: #5f07ff;
 
   display: flex;
   justify-content: center;
 }
-.row{
-   background: #5f07ff;
-  width: 300px;
-  height: 400px;
-  border-radius: 41px;
-  margin-top: 30px;
 
-  display: flex;
-  justify-content: center;
-}
-#email{
-  background: #ffffff1f;
-  width: 255px;
-  height: 40px;
-  border-radius: 7px;
-  margin-top: 150px;
-  border-color: #ffffff;
-  margin-left: 15px;
-
-  display: flex;
-  justify-content: center;
-}
-#senha{
-  background: #ffffff00;
-  width: 255px;
-  height: 40px;
-  border-radius: 7px;
-  margin-top: 27px;
-  margin-left: 15px;
-   border-color: #ffffff;
-
-  
-  justify-content: center;
+.logo {
+  position: absolute;
+  top: 70px;
+  left: 75px;
 }
 
-#texto{
-  color: #ffffff5b;
-  margin-top:10px;
-  margin-left: -150px;
+.form {
+  position: absolute;
+  top: 160px;
+  left: 37px;
 }
 
+.form input {
+  width: 275px;
+  height: 45px;
+  margin: 3px 0;
+  padding: 10px;
 
+  background-color: #f0e4e4;
+  border: 1px solid #ffffff;
+  box-sizing: border-box;
+  border-radius: 10px;
+}
+
+.button-group {
+  width: 200px;
+  margin-top: 20px;
+}
+
+.button-group button {
+  position: absolute;
+  border-radius: 10px;
+  color: #FFFFFF;
+}
+
+.button-entrar {
+  width: 200px;
+  height: 35px;
+  left: 37px;
+
+  font-size: 15px;
+  background: #11C891;
+}
+
+.button-cadastrar {
+  width: 150px;
+  height: 25px;
+  margin-top: 42px;
+  left: 60px;
+
+  font-size: 12px;
+  line-height: 12px;
+  background: #fd3f3f;
+}
 </style>
