79f72d69bb9769f869d50c42bf501a720bd23d31
TheAldo <47190980+TheAldo@users.noreply.github.com>
2021-05-25 23:29:36 -0300

    Update Audio.vue

--- frontend/src/views/Audio.vue
+++ frontend/src/views/Audio.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-  <audio src="audio.mp3"></audio>
+  <audio src="@/audio.mp3"></audio>
   <button>play</button>
+  <button>pause</button>
    </div>
 
    
 </template>
+<script>
+
+
+const button = document.querySelector('button')
+button.addEventListener('click', function(){
+  const audio = document.querySelector('audio')
+  audio.play()
+})
+</script>
+
 
 
 <script>
-
 
 export default {
   name: "Audio",
