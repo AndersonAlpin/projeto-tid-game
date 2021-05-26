48a12b41cb23cc1aaccac771ec765dfc50fba338
TheAldo <47190980+TheAldo@users.noreply.github.com>
2021-05-26 00:27:19 -0300

    front

---
+++ frontend/src/views/audio.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+  <audio src="@/assests/audio/audio.mp3"></audio>
+  <button onclik=play()>play</button>
+  <button oncli=pause() id="pause">pause</button>
+   </div>
+
+   
+</template>
+<script>
+
+
+const button = document.querySelector('button')
+button.addEventListener('click', function play(){
+  const audio = document.querySelector('audio')
+  audio.play()
+
+})
+
+
+</script>
+
+
+
+<script>
+
+export default {
+  name: "Audio",
+};
+
+    
+  
+  
+</script>
+
+
+
+
+
+<style>
+
+</style>
