08b42faa1fb1fc1e341a9d3fd5fa5b95db8ca6de
AndersonAlpin <andersonalpin@hotmail.com>
2021-06-01 23:04:47 -0300

    Base da nova versão

---
+++ frontend/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../views/Login.vue'
+import Game from '../views/Game.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/game',
+    name: 'Game',
+    component: Game
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
